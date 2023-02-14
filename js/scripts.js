79ea412fc03e169a3837f89791acd6765d47dc50
danielpaik1 <danielpaik1@gmail.com>
2023-02-13 23:14:53 -0500

    attempted: api . Failed
    tried looking at peer work and follow the code but failed. was advised to look at add function with eventlistener. result: nothing. trying to display pokemon img on modal

--- js/scripts.js
+++ js/scripts.js
@@ -1,67 +1,73 @@
 let pokemonRepository = (function () {
-  
-  let pokemonList = [];                   //variable 1 
-  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';  //variable 2
+    let pokemonList = [];                   //variable 1 
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';  //variable 2
 
-//function to add a pokemon to the pokemonList array
+    //function to add a pokemon to the pokemonList array
  
- function add(pokemon) {
-    pokemonList.push(pokemon);
+    function add(pokemon) {
+        pokemonList.push(pokemon);
   }
   
- function getAll () { //this will return pokemon array 
-    return pokemonList;
+    function getAll () { //this will return pokemon array 
+        return pokemonList;
   }
   
- function addListItem(pokemon){
-    let pokemonlist = document.querySelector(".pokemon-list");
-    let listpokemon = document.createElement("li");
-    let button = document.createElement('button');
-    button.innerText = pokemon.name;
-    button.classList.add('button-class'); //create a CSS rule that targets this class and give it a custom button style ofy your own to overwrite the defualt styling 
-    listpokemon.appendChild(button);
-    pokemonlist.appendChild(listpokemon);
-    button.addEventListener('click', function () {     
-      showDetails(pokemon);
-                                                
-    });
- }
+    function addListItem(pokemon){
+        let pokemonList = document.querySelector(".pokemon-list");
+        let listPokemon = document.createElement("li");
+        let button = document.createElement('button');
+        button.innerText = pokemon.name;
+        button.classList.add('button-class'); //create a CSS rule that targets this class and give it a custom button style ofy your own to overwrite the defualt styling 
+        listPokemon.appendChild(button);
+        pokemonRepository.appendChild(listPokemon);
+        button.addEventListener('click', 
+        function (Event) {     
+          showDetails(pokemon);                                     
+        });
+    }
   //this promise function is a fetch function
- 
-    
-function loadList() {
-    return fetch(apiUrl).then(function (response) {
-    return response.json();
-    }).then(function (json) {
-      json.results.forEach(function (item) {
-        let pokemon = {
-          name: item.name,
-          detailsUrl: item.url
-        };
-        add(pokemon);
-        console.log(pokemon);
-      });
-    }).catch(function (e) {
-      console.error(e);
-    })
-  }
+    function loadList() {
+        return fetch(apiUrl)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (json) {
+          json.results.forEach(function (item) {
+            let pokemon = {
+              name: item.name,
+              detailsUrl: item.url
+            };
+            add(pokemon);
+          });
+        })
+        .catch(function (e) {
+          console.error(e);
+        });
+    }
 
-  function loadDetails(item) { //loadDetail is a variable witha parameter "item" 
-    let url = item.detailsUrl;
-    return fetch(url).then(function (response) {
-      return response.json();
-    }).then(function (details) {
-      // Now we add the details to the item
-      item.imageUrl = details.sprites.front_default;
-      item.height = details.height;
-      item.types = details.types;
-    }).catch(function (e) {
-      console.error(e);
+      function loadDetails(item) { //loadDetail is a variable witha parameter "item" 
+        let url = item.detailsUrl;
+          return fetch(url)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (details) {
+          // Now we add the details to the item
+          item.imageUrl = details.sprites.front_default;
+          item.height = details.height;
+          item.types = details.types;
+        })
+        .catch(function (e) {
+          console.error(e);
+        });
+      }
+
+
+  function showDetails(item) {
+    pokemonRepository.loadDetails(item).then(function () {
+      console.log(item);
     });
   }
-
-
-  function showDetails(pokemon) {
       let modalContainer = document.querySelector('#modal-container');
   
   function showModal(title, text) {
@@ -110,10 +116,10 @@ function loadList() {
       showModal(
         pokemon.name,
         pokemon.name + "'s height is: " + pokemon.height,
-        pokemon.imageURL
+        pokemon.imageUrl
       );
     });
-  }                                      
+                                       
   
   return {
     add: add,
@@ -124,7 +130,7 @@ function loadList() {
     showDetails: showDetails 
   };
 
-}) ();
+})();
 
   pokemonRepository.loadList().then(function() { 
     pokemonRepository.getAll().forEach(function(pokemon) {
